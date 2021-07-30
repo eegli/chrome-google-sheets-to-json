9a78412df4a12e7ef560abe5c0604d7cf43ec146
Eric <43848365+eegli@users.noreply.github.com>
2021-07-30 21:59:10 +0200

    webpack env variables

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,51 +3,52 @@ import * as webpack from 'webpack';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const config: webpack.Configuration = {
-  mode: 'production',
-  entry: {
-    background: './src/background.ts',
-    popup: './src/popup/index.tsx',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx'],
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [{ from: 'manifest.json' }],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+const config = (env: any): webpack.Configuration => {
+  const isProd = !!env.prod;
+  return {
+    mode: 'production',
+    entry: {
+      background: './src/background.ts',
+      popup: './src/popup/index.tsx'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      clean: true
+    },
+    module: {
+      rules: [
         {
-          from: './public/assets',
-          to: './assets',
-          globOptions: { ignore: ['**/*.psd'] },
-        },
-      ],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'popup.html',
-      template: './public/popup.html',
-      chunks: ['popup'],
-    }),
-  ],
-  optimization: {
-    minimize: false,
-  },
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx']
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'manifest.json' }]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: './public/assets',
+            to: './assets',
+            globOptions: { ignore: ['**/*.psd'] }
+          }
+        ]
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'popup.html',
+        template: './public/popup.html',
+        chunks: ['popup']
+      })
+    ],
+    stats: isProd ? 'normal' : 'minimal'
+  };
 };
 
 export default config;
